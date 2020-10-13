ac6202d41bde0d114ca64f7630a305319ea8fb5e
Simranjeet Singh <simranjeetsingh@ip-192-168-1-3.ap-south-1.compute.internal>
2020-10-13 10:48:54 +0530

    Updates

---
+++ precache-manifest.ff88c8266d8a5817de6ace1bb31e0c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96b49b9db1db666c90cecaf87a4bb55",
+    "url": "/drawingboard/index.html"
+  },
+  {
+    "revision": "1a06211bbd678b323f4b",
+    "url": "/drawingboard/static/css/main.0928dc50.chunk.css"
+  },
+  {
+    "revision": "6015bef0dd2241dd896a",
+    "url": "/drawingboard/static/js/2.8c2df445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drawingboard/static/js/2.8c2df445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a06211bbd678b323f4b",
+    "url": "/drawingboard/static/js/main.4e312798.chunk.js"
+  },
+  {
+    "revision": "18efd9b3756dd0c97f33",
+    "url": "/drawingboard/static/js/runtime-main.de820e33.js"
+  }
+]);
